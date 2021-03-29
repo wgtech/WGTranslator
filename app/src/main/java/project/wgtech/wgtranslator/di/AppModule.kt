6aa4b2c56929b071f4777a0f3df85dd2d989e5a3
WangGi Lim <wgtech.dev@gmail.com>
2021-03-30 00:26:36 +0900

    Add @Provides method to use InputMethodManager, Google Translate Managers

--- app/src/main/java/project/wgtech/wgtranslator/di/AppModule.kt
+++ app/src/main/java/project/wgtech/wgtranslator/di/AppModule.kt
@@ -2,11 +2,15 @@ package project.wgtech.wgtranslator.di
 
 import android.content.ClipboardManager
 import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.common.model.RemoteModelManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import project.wgtech.wgtranslator.R
 import retrofit2.Retrofit
 
 @InstallIn(SingletonComponent::class)
@@ -15,8 +19,19 @@ object AppModule {
 
     @Provides
     fun provideClipboardManager(@ApplicationContext context: Context): ClipboardManager =
-        context.applicationContext.getSystemService(Context.CLIPBOARD_SERVICE)
-                as ClipboardManager
+        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+    @Provides
+    fun provideInputMethodManager(@ApplicationContext context: Context): InputMethodManager =
+        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    @Provides
+    fun provideRemoteModelManager() : RemoteModelManager =
+        RemoteModelManager.getInstance()
+
+    @Provides
+    fun provideDownloadConditions() : DownloadConditions =
+        DownloadConditions.Builder().requireWifi().build()
 
     @Provides
     internal fun provideRetrofitClient() : Retrofit =
