a2ee3a2733de8604ba656c1d6964e09df5d82bf8
WangGi Lim <wgtech.dev@gmail.com>
2021-03-30 00:31:12 +0900

    Apply MultiDex to application with support lower versions

--- app/src/main/java/project/wgtech/wgtranslator/GlobalApplication.kt
+++ app/src/main/java/project/wgtech/wgtranslator/GlobalApplication.kt
@@ -1,6 +1,8 @@
 package project.wgtech.wgtranslator
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -8,5 +10,10 @@ class GlobalApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
