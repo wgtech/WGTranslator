826b97eb3aaa98d10be7a4102cbc9dac1b601537
WangGi Lim <wgtech.dev@gmail.com>
2021-04-01 22:10:26 +0900

    Add CoroutinesQualifiers and apply them for DI

--- app/src/main/java/project/wgtech/wgtranslator/di/CoroutinesModule.kt
+++ app/src/main/java/project/wgtech/wgtranslator/di/CoroutinesModule.kt
@@ -11,15 +11,19 @@ import kotlinx.coroutines.Dispatchers
 @Module
 object CoroutinesModule {
 
+    @DefaultDispatcher
     @Provides
     fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
+    @IoDispatcher
     @Provides
     fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
+    @MainDispatcher
     @Provides
     fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 
+    @MainImmediateDispatcher
     @Provides
     fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
 }
